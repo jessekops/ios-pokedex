b9d5b4d00540c69fe2d5fb116d2cab1dd453799c
jessekops <645147@student.inholland.nl>
2024-11-18 14:01:16 +0100

    first commit

---
+++ student645147/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  student645147
+//
+//  Created by user271057 on 11/18/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ student645147/student645147App.swift
@@ -0,0 +1,17 @@
+//
+//  student645147App.swift
+//  student645147
+//
+//  Created by user271057 on 11/18/24.
+//
+
+import SwiftUI
+
+@main
+struct student645147App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
